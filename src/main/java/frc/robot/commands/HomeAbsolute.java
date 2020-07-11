c4cc651900858908426dfd220749e59272f3a910
Alexander McDowell <lextmcdowell@gmail.com>
2020-07-11 14:56:26 -0700

    Changed HomeAbsolute to an instant command

--- src/main/java/frc/robot/commands/HomeAbsolute.java
+++ src/main/java/frc/robot/commands/HomeAbsolute.java
@@ -7,28 +7,20 @@
 
 package frc.robot.commands;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.Constants;
 import frc.robot.Robot;
 
-public class HomeAbsolute extends Command {
-
-  boolean frZero = false;
-  boolean flZero = false;
-  boolean blZero = false;
-  boolean brZero = false;
-
-  double margin;
-  double speed;
+public class HomeAbsolute extends InstantCommand {
+  double flQuadPos;
+  double frQuadPos;
+  double blQuadPos;
+  double brQuadPos;
 
   public HomeAbsolute() {
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.drivetrain);
   }
   
   // Called just before this Command runs the first time
@@ -46,67 +38,17 @@ public class HomeAbsolute extends Command {
     Robot.drivetrain.frederickTurn.setSensorPhase(true);
     Robot.drivetrain.brianTurn.setSensorPhase(true);
 
-    SmartDashboard.putNumber("HomeAbsolute margin", 1.0);
-    SmartDashboard.putNumber("HomeAbsolute speed", 0.05);
-  }
-
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
-    // Get the new margin and speed values.
-    margin = SmartDashboard.getNumber("HomeAbsolute margin", 1.0);
-    speed = SmartDashboard.getNumber("HomeAbsolute speed", 0.05);
-
-    // Use copySign to make sure that the motor controller steers in the direction of the setpoint
-    if (!frZero) {
-      Robot.drivetrain.swerveEnclosureFR.move(0.0,
-        Math.copySign(speed, Constants.FR_TURN_ZERO - Robot.drivetrain.frederickTurn.getSensorCollection().getAnalogIn()));
-    }
-    if (!flZero)  {
-      Robot.drivetrain.swerveEnclosureFL.move(0.0,
-        Math.copySign(speed, Constants.FL_TURN_ZERO - Robot.drivetrain.fletcherTurn.getSensorCollection().getAnalogIn()));
-    }
-    if (!blZero) {
-      Robot.drivetrain.swerveEnclosureBL.move(0.0,
-        Math.copySign(speed, Constants.BL_TURN_ZERO - Robot.drivetrain.blakeTurn.getSensorCollection().getAnalogIn()));
-    }
-    if (!brZero) {
-      Robot.drivetrain.swerveEnclosureBR.move(0.0,
-        Math.copySign(speed, Constants.BR_TURN_ZERO - Robot.drivetrain.brianTurn.getSensorCollection().getAnalogIn()));
-    }
-  }
-
-  // Checks if each motor controller has turned until it is close to some known setpoint.
-  @Override
-  protected boolean isFinished() {
-    // Check if a motor controller is within a specified margin of the target value.
-    flZero = (Math.abs(Robot.drivetrain.frederickTurn.getSensorCollection().getAnalogIn() - Constants.FR_TURN_ZERO) <= margin);
-    flZero = (Math.abs(Robot.drivetrain.fletcherTurn.getSensorCollection().getAnalogIn() - Constants.FL_TURN_ZERO) <= margin);
-    blZero = (Math.abs(Robot.drivetrain.blakeTurn.getSensorCollection().getAnalogIn() - Constants.BL_TURN_ZERO) <= margin);
-    brZero = (Math.abs(Robot.drivetrain.brianTurn.getSensorCollection().getAnalogIn() - Constants.BR_TURN_ZERO) <= margin);
-
-    // If a motor controller is in the right position, stop moving it.
-    if (frZero) Robot.drivetrain.frederickTurn.set(ControlMode.PercentOutput, 0.0);
-    if (flZero) Robot.drivetrain.fletcherTurn.set(ControlMode.PercentOutput, 0.0);
-    if (blZero) Robot.drivetrain.blakeTurn.set(ControlMode.PercentOutput, 0.0);
-    if (brZero) Robot.drivetrain.brianTurn.set(ControlMode.PercentOutput, 0.0);
-
-    return flZero && frZero && blZero && brZero;
-  }
-
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-    // Zero the quadrature encoders.
-    Robot.drivetrain.fletcherTurn.setSelectedSensorPosition(0);
-    Robot.drivetrain.blakeTurn.setSelectedSensorPosition(0);
-    Robot.drivetrain.frederickTurn.setSelectedSensorPosition(0);
-    Robot.drivetrain.brianTurn.setSelectedSensorPosition(0);
-  }
-
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
+    // Change the current quadrature encoder position to the difference between the zeroed position and the current position, as measured by the analog encoder.
+    // Difference is in analog encoder degrees which must be converted to quadrature encoder ticks.
+    // Max value of the analog encoder is 1023, min value is 0.
+    flQuadPos = (Constants.FL_GEAR_RATIO / 1024.0) * (Constants.FL_TURN_ZERO - Robot.drivetrain.fletcherTurn.getSensorCollection().getAnalogIn());
+    frQuadPos = (Constants.FR_GEAR_RATIO / 1024.0) * (Constants.FR_TURN_ZERO - Robot.drivetrain.frederickTurn.getSensorCollection().getAnalogIn());
+    blQuadPos = (Constants.BL_GEAR_RATIO / 1024.0) * (Constants.BL_TURN_ZERO - Robot.drivetrain.blakeTurn.getSensorCollection().getAnalogIn());
+    brQuadPos = (Constants.BR_GEAR_RATIO / 1024.0) * (Constants.BR_TURN_ZERO - Robot.drivetrain.brianTurn.getSensorCollection().getAnalogIn());
+    
+    Robot.drivetrain.fletcherTurn.setSelectedSensorPosition((int) flQuadPos);
+    Robot.drivetrain.frederickTurn.setSelectedSensorPosition((int) frQuadPos);
+    Robot.drivetrain.blakeTurn.setSelectedSensorPosition((int) blQuadPos);
+    Robot.drivetrain.brianTurn.setSelectedSensorPosition((int) brQuadPos);
   }
 }
